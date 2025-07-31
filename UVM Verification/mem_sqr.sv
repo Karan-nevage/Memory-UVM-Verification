7f18cc29c346f819996960c5f935be1beaafa385
Karankumar Nevage <111281049+Karan-nevage@users.noreply.github.com>
2025-08-01 00:53:25 +0530

    SEQUENCER

---
+++ UVM Verification/mem_sqr.sv
@@ -0,0 +1,6 @@
+// File: mem_sqr.sv
+// Description: Defines the sequencer typedef for memory transactions.
+// Sequencer typedef for memory transactions
+//=======================================================================
+typedef uvm_sequencer#(mem_tx) mem_sqr;
+//=======================================================================
